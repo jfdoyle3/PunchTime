f31d1edda56ae236bae18e69a5da2536ce20a86a
jfdoyle3 <36973560+jfdoyle3@users.noreply.github.com>
2022-02-12 14:45:05 -0500

    added project

---
+++ punchtime/src/main/java/com/jdeveloper/punchtime/PunchtimeApplication.java
@@ -0,0 +1,13 @@
+package com.jdeveloper.punchtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunchtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunchtimeApplication.class, args);
+	}
+
+}
