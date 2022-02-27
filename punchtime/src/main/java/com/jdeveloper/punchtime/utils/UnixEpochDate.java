c7797e88e147b3116d13a106088d6f08abb027ad
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-02-27 18:16:09 -0500

    create UnixEpochDate class

---
+++ punchtime/src/main/java/com/jdeveloper/punchtime/utils/UnixEpochDate.java
@@ -0,0 +1,71 @@
+package com.jdeveloper.punchtime.utils;
+
+public class UnixEpochDate {
+
+	
+	private String datein;
+	private String dateOut;
+	private long unixTimeIn;
+	private long unixTimeOut;
+	private double hours;
+	
+	
+	public UnixEpochDate(String datein, String dateOut) {
+		super();
+		this.datein = datein;
+		this.dateOut = dateOut;
+	}
+
+
+	public String getDatein() {
+		return datein;
+	}
+
+
+	public void setDatein(String datein) {
+		this.datein = datein;
+	}
+
+
+	public String getDateOut() {
+		return dateOut;
+	}
+
+
+	public void setDateOut(String dateOut) {
+		this.dateOut = dateOut;
+	}
+
+
+	public long getUnixTimeIn() {
+		return unixTimeIn;
+	}
+
+
+	public void setUnixTimeIn(long unixTimeIn) {
+		this.unixTimeIn = unixTimeIn;
+	}
+
+
+	public long getUnixTimeOut() {
+		return unixTimeOut;
+	}
+
+
+	public void setUnixTimeOut(long unixTimeOut) {
+		this.unixTimeOut = unixTimeOut;
+	}
+
+
+	public double getHours() {
+		return hours;
+	}
+
+
+	public void setHours(double hours) {
+		this.hours = hours;
+	}
+	
+	
+	
+}
