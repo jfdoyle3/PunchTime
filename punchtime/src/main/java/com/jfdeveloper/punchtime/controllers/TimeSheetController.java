86958f04cb1b2503669bf5d08ceb4cfa93964d90
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-03-11 20:48:56 -0500

    Working on timesheet

---
+++ punchtime/src/main/java/com/jfdeveloper/punchtime/controllers/TimeSheetController.java
@@ -0,0 +1,47 @@
+package com.jfdeveloper.punchtime.controllers;
+
+import java.text.ParseException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jfdeveloper.punchtime.entities.TimeSheet;
+import com.jfdeveloper.punchtime.repositories.TimeSheetRepository;
+import com.jfdeveloper.punchtime.utils.DateTimeUtils;
+
+
+@RestController
+@RequestMapping("/api/timesheets")
+public class TimeSheetController {
+	
+	@Autowired
+	private TimeSheetRepository repository;
+	
+	
+	@GetMapping
+	@ResponseBody
+	public List<TimeSheet> getTimesheet() { return repository.findAll(); } 
+	
+	@PostMapping
+	public ResponseEntity<TimeSheet> addTimeSheet(@RequestBody TimeSheet newTimeSheet) throws ParseException {
+		newTimeSheet.setTotalHours(DateTimeUtils.calculateHours(newTimeSheet.getStartDate(),newTimeSheet.getEndDate()));
+		return new ResponseEntity<>(repository.save(newTimeSheet), HttpStatus.CREATED);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> removeShift(@PathVariable Long id) {
+		repository.deleteById(id);
+		return new ResponseEntity<>("Shift ID " + id + " removed", HttpStatus.OK);
+	}
+	
+}
--- punchtime/src/main/java/com/jfdeveloper/punchtime/entities/TimeSheet.java
+++ punchtime/src/main/java/com/jfdeveloper/punchtime/entities/TimeSheet.java
@@ -10,14 +10,78 @@ public class TimeSheet {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	Long id;
+	private Long id;
+	private String employee;
+	private String company;
+	private String jobTitle;
+	private Long startDate;
+	private Long endDate;
+	private Double totalHours;
+	
+	public TimeSheet() {}	
 		
-	// Employee
-	// Company
-	// Position / Department
-	// Week Start
-	// Week Ending
-	// Total Hours
-	
-	
+	public TimeSheet(String employee, String company, String jobTitle, Long startDate, Long endDate,
+			Double totalHours) {
+		this.employee = employee;
+		this.company = company;
+		this.jobTitle = jobTitle;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(String employee) {
+		this.employee = employee;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getJobTitle() {
+		return jobTitle;
+	}
+
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+
+	public Long getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Long startDate) {
+		this.startDate = startDate;
+	}
+
+	public Long getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Long endDate) {
+		this.endDate = endDate;
+	}
+
+	public Double getTotalHours() {
+		return totalHours;
+	}
+
+	public void setTotalHours(Double totalHours) {
+		this.totalHours = totalHours;
+	}
 }
