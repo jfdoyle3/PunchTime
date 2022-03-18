d18851afbc72a4b19e8bd33ce419f47a1a4b33c7
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-03-17 22:32:49 -0400

    cleaned out the repository classes.

--- punchtime/src/main/java/com/jfdeveloper/punchtime/repositories/CompanyRepository.java
+++ punchtime/src/main/java/com/jfdeveloper/punchtime/repositories/CompanyRepository.java
@@ -4,13 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.jfdeveloper.punchtime.entities.Company;
-import com.jfdeveloper.punchtime.entities.Shift;
 
 @Repository
 	public interface CompanyRepository extends JpaRepository<Company, Long> {
-
-	Object save(Shift newCompany);
-	
-	
 	}
 
